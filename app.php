44c3dede93ddc0ebd9355440a502b9581b345f38
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-27 16:52:38 -0500

    feat: :sparkles: Ejercicio 6 php
    solucion del ejercicio 6 de la pagina

--- app.php
+++ app.php
@@ -1,11 +1,23 @@
-//?Introduction
-//?The way we measure time is kind of messy. We have 60 seconds in a minute, and 60 minutes in an hour. This comes from ancient Babylon, where they used 60 as the basis for their number system. We have 24 hours in a day, 7 days in a week, and how many days in a month? Well, for days in a month it depends not only on which month it is, but also on what type of calendar is used in the country you live in.
+//?Instructions
+//?Tally the results of a small football competition.
 //?
-//?What if, instead, we only use seconds to express time intervals? Then we can use metric system prefixes for writing large numbers of seconds in more easily comprehensible quantities.
+//?Based on an input file containing which team played against which and what the outcome was, create a file with a table like this:
 //?
-//?A food recipe might explain that you need to let the brownies cook in the oven for two kiloseconds (that's two thousand seconds).
-//?Perhaps you and your family would travel to somewhere exotic for two megaseconds (that's two million seconds).
-//?And if you and your spouse were married for a thousand million seconds, you would celebrate your one gigasecond anniversary.
+//?Team                           | MP |  W |  D |  L |  P
+//?Devastating Donkeys            |  3 |  2 |  1 |  0 |  7
+//?Allegoric Alaskans             |  3 |  2 |  0 |  1 |  6
+//?Blithering Badgers             |  3 |  1 |  0 |  2 |  3
+//?Courageous Californians        |  3 |  0 |  1 |  2 |  1
+//?What do those abbreviations mean?
+//?
+//?MP: Matches Played
+//?W: Matches Won
+//?D: Matches Drawn (Tied)
+//?L: Matches Lost
+//?P: Points
+//?A win earns a team 3 points. A draw earns 1. A loss earns 0.
+//?
+//?The outcome should be ordered by points, descending. In case of a tie, teams are ordered alphabetically.
 <?php
 
 /*
@@ -32,11 +44,84 @@
 
 declare(strict_types=1);
 
-function from(DateTimeImmutable $date): DateTimeImmutable
-{
-    $gigaSecond = new DateInterval('PT1000000000S');
-    $futureDate = $date->add($gigaSecond);
-    
-    return $futureDate;
-}
-?>
+class Tournament
+    {
+        public $MP = [];
+        public $W = [];
+        public $D = [];
+        public $L = [];
+        public $P = [];
+        public $equipos;
+        public function __construct($scores){
+            $this->equipos = explode(";", $scores);
+
+        }
+        public function asignacionPuntos(){
+            foreach ($this->equipos as $key => $value) {
+                if($key%3 == 2){
+                    switch ($this->equipos[$key]) {
+                        case 'win':
+                            $nombreEquipo = $this->equipos[$key-2];
+                            $nombreEquipo2 = $this->equipos[$key-1];
+                            ($this->W[$nombreEquipo] ?? null) ? $this->W[$nombreEquipo] += 1 : $this->W[$nombreEquipo] = 1;
+                            ($this->L[$nombreEquipo2] ?? null) ? $this->L[$nombreEquipo2] += 1 : $this->L[$nombreEquipo2] = 1;
+                            ($this->P[$nombreEquipo] ?? null) ? $this->P[$nombreEquipo] += 3 : $this->P[$nombreEquipo] = 3;
+                            break;
+                        case 'draw':
+                            $nombreEquipo = $this->equipos[$key-2];
+                            $nombreEquipo2 = $this->equipos[$key-1];
+                            ($this->D[$nombreEquipo] ?? null) ? $this->D[$nombreEquipo] += 1 : $this->D[$nombreEquipo] = 1;
+                            ($this->D[$nombreEquipo2] ?? null) ? $this->D[$nombreEquipo2] += 1 : $this->D[$nombreEquipo2] = 1;
+
+                            ($this->P[$nombreEquipo] ?? null) ? $this->P[$nombreEquipo] += 1 : $this->P[$nombreEquipo] = 1;
+                            ($this->P[$nombreEquipo2] ?? null) ? $this->P[$nombreEquipo2] += 1 : $this->P[$nombreEquipo2] = 1;
+                            break;
+                        case 'loss':
+                            $nombreEquipo = $this->equipos[$key-1];
+                            $nombreEquipo2 = $this->equipos[$key-2];
+                            ($this->W[$nombreEquipo] ?? null) ? $this->W[$nombreEquipo] += 1 : $this->W[$nombreEquipo] = 1;
+                            ($this->L[$nombreEquipo2] ?? null) ? $this->L[$nombreEquipo2] += 1 : $this->L[$nombreEquipo2] = 1;
+                            ($this->P[$nombreEquipo] ?? null) ? $this->P[$nombreEquipo] += 3 : $this->P[$nombreEquipo] = 3;
+                            break;
+                    }
+                }else{
+                    ($this->MP[$this->equipos[$key]] ?? null) ? $this->MP[$this->equipos[$key]] += 1 : $this->MP[$this->equipos[$key]] = 1;
+                }
+            }
+        }
+        public function validarEquipos(){
+            $equiposFaltantesW = array_diff_key($this->MP, $this->W);
+            foreach ($equiposFaltantesW as $key => $value) {
+                $this->W[$key] = 0;
+            }
+            $equiposFaltantesD = array_diff_key($this->MP, $this->D);
+            foreach ($equiposFaltantesD as $key => $value) {
+                $this->D[$key] = 0;
+            }
+            $equiposFaltantesL = array_diff_key($this->MP, $this->L);
+            foreach ($equiposFaltantesL as $key => $value) {
+                $this->L[$key] = 0;
+            }
+            $equiposFaltantesP = array_diff_key($this->MP, $this->P);
+            foreach ($equiposFaltantesP as $key => $value) {
+                $this->P[$key] = 0;
+            }
+        }
+        public function tablaResultados(){
+            echo "        Team                  | MP | W  | D  | L  | P  |\n  ";
+            echo "---------------------------------------------------\n";
+            foreach ($this->MP as $equipo => $partidosJugados){
+                echo str_pad($equipo, 30)."| ";
+                echo str_pad(strval($partidosJugados), 3)."| ";
+                echo str_pad(($this->W[strval([$equipo]) ?? 0),3)."| ";
+                echo str_pad(($this->D[$equipo] ?? 0),3)."| ";
+                echo str_pad(($this->L[$equipo] ?? 0),3)."| ";
+                echo str_pad(($this->P[$equipo] ?? 0),3)."| "."\n";
+            }
+        }
+    }
+    $obj = new Tournament("Allegoric Alaskans;Blithering Badgers;win;Devastating Donkeys;Courageous Californians;draw;Devastating Donkeys;Allegoric Alaskans;win;Courageous Californians;Blithering Badgers;loss;Blithering Badgers;Devastating Donkeys;loss;Allegoric Alaskans;Courageous Californians;win");
+
+    $obj->asignacionPuntos();
+    $obj->validarEquipos();
+    $obj->tablaResultados();
